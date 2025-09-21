4637df6d56482d8cd2390c50bc64f1f5cd3e16be
garg-lakshay <lakshaygarg775@gmail.com>
2025-09-21 23:03:12 +0530

    nested-layout

---
+++ src/app/product/[productId]/layout.tsx
@@ -0,0 +1,15 @@
+export default function ProductLayout({
+    children,
+}:{
+    children:React.ReactNode;
+}) {
+    return (
+        <>
+        {children}
+        <h2>Featured products</h2>
+        </>
+    );
+}
+
+
+
