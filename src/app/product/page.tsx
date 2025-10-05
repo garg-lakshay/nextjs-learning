ea1c3526d146911a80c5b3f0a29c213a80fe6897
garg-lakshay <lakshaygarg775@gmail.com>
2025-10-06 01:33:11 +0530

    Link component navigation

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
 export default function Product(){
+    const productId = 100;
     return (
         <>
         <h1>Product List</h1>
-        <h2>Product1</h2>
-        <h2>Product2</h2>
-        <h2>Product3</h2>
+        <h2><Link href="/product/1">Product1</Link></h2>
+        <h2><Link href="/product/2">Product2</Link></h2>
+        <h2><Link href="/product/3">Product3</Link></h2>
+        <h2><Link href={`/product/${productId}`}>Product{productId}</Link></h2>
+
+        <Link href="/">Back to Home</Link>
         </>
     );
 }
