d7bd14793ed0618a9e07b1fdac4b499225bb645e
garg-lakshay <lakshaygarg775@gmail.com>
2025-10-07 01:37:42 +0530

    check active links

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -6,7 +6,7 @@ export default function Product(){
         <h1>Product List</h1>
         <h2><Link href="/product/1">Product1</Link></h2>
         <h2><Link href="/product/2">Product2</Link></h2>
-        <h2><Link href="/product/3">Product3</Link></h2>
+        <h2><Link href="/product/3 " replace>Product3</Link></h2>
         <h2><Link href={`/product/${productId}`}>Product{productId}</Link></h2>
 
         <Link href="/">Back to Home</Link>
