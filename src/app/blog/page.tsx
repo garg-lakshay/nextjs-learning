a0c9262b578a53905ad15ff1748ed1811c6b8385
garg-lakshay <lakshaygarg775@gmail.com>
2025-10-09 00:34:59 +0530

    Error-Handaling

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,21 @@
 import {Metadata} from "next";
 
+
+
 export const metadata: Metadata = {
     title:{
         absolute:"Blog",
     }
 };
 
-export default function Blog(){
+export default async function Blog(){
+    await new Promise((resolve) => {
+        setTimeout(() =>{
+            resolve("intentional delay");
+        }, 2000);
+
+    
+    });
     return <h1> my Blog </h1>
+
 }
