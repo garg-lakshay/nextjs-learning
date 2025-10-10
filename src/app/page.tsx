d88835b1a02f85002136f15c0576f9381878bf40
garg-lakshay <lakshaygarg775@gmail.com>
2025-10-10 21:09:21 +0530

    handling error in layout and global errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ export default function Home(){
     return (
     <>
     <h1>Welcome home!</h1>
-    <Link href="/blog">Blog</Link>
-    <Link href="/product">Products</Link>
-    <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
-    <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
+    <Link href="/blog">Blog</Link><br />
+
+    <Link href="/product">Products</Link><br />
+
+    <Link href="/articles/breaking-news-123?lang=en">Read in English</Link><br />
+
+    <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link><br />
     </>
     ); 
 }
--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,19 +1,20 @@
 import {notFound , redirect } from "next/navigation";
 
-function getRandomInt(count: number){
-    return Math.floor(Math.random() * count);   
+// function getRandomInt(count: number){
+//     return Math.floor(Math.random() * count);   
 
-} 
+// } 
 
 export  default async function ProductReview({
     params,
 }:{
     params: Promise<{ productId: string; reviewId: string}>;
 }){
-    const random  = getRandomInt(2);
-    if(random ==0){
-        throw new Error("Error loading review");
-    }
+    // const random  = getRandomInt(2);
+    // console.log("Random number is", random);
+    // if(random ==1){
+    //     throw new Error("Error loading review");
+    // }
     const {productId, reviewId} = await params
     if(parseInt(reviewId) > 100){
         //notFound();
