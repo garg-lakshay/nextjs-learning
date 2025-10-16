a5d68f8aa04a14733c6e6dff1a0a61908a4cf5a1
garg-lakshay <lakshaygarg775@gmail.com>
2025-10-16 23:57:27 +0530

    refactored-parallel-intercepted-routes

---
+++ src/app/photo-feed/layout.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function Layout(props:
+    {
+        modal: React.ReactNode;
+        children: React.ReactNode;
+    }){
+        return(
+            <>
+            {props.modal}
+            {props.children}
+            </>
+        ); 
+    }
+
